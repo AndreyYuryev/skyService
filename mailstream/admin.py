312cdb104c941301b1b77167c411da37267cac81
Andrey <yuryev@yandex.ru>
2024-01-03 22:39:10 +0300

    model Stream

--- mailstream/admin.py
+++ mailstream/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Client
+from models import Client, Stream
 
 
 @admin.register(Client)
@@ -7,3 +7,10 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ('fullname', 'email',)
     list_filter = ('fullname', 'email',)
     search_fields = ('fullname',)
+
+
+@admin.register(Stream)
+class StreamAdmin(admin.ModelAdmin):
+    list_display = ('regularity', 'status', 'post_time',)
+    list_filter = ('regularity', 'status',)
+    search_fields = ('regularity', 'status', )
--- mailstream/models.py
+++ mailstream/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
+REGULARITY_VALUES = [('d', 'раз в день'), ('w', 'раз в неделю', 'm', 'раз в месяц'), ]
+STATUS_VALUES = [('l', 'завершена'), ('r', 'создана'), ('s', 'запущена'), ]
 
 
 class Client(models.Model):
@@ -14,3 +16,16 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'Клиент сервиса'
         verbose_name_plural = 'Клиенты сервиса'
+
+
+class Stream(models.Model):
+    post_time = models.DateTimeField()
+    regularity = models.CharField(max_length='1', choices=REGULARITY_VALUES, verbose_name='Периодичность')
+    status = models.CharField(max_length='1', choices=STATUS_VALUES, verbose_name='Статус рассылки')
+
+    def __str__(self):
+        return f'{self.status} - {self.regularity}'
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
